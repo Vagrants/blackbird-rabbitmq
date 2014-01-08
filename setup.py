9fa7754f491628dcfc97c9f1d10dc0d70459e84a
makocchi <makocchi@gmail.com>
2014-01-08 21:40:00 +0900

    add require python-requests

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='blackbird-rabbitmq',
-    version='0.1.2',
+    version='0.1.3',
     description=(
         'Get monitorring stats of rabbitmq for blackbird '
     ),
